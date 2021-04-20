d0052575b122a66d0885338b835536ce4c2858f6
JoeRojas <josuerojasq@gmail.com>
2021-04-20 16:21:30 -0400

    Voy a renombrar esta carpeta

--- 102.misPaquetes/progs/main.py
+++ 102.misPaquetes/progs/main.py
@@ -2,8 +2,6 @@ from sys import path
 
 path.append('home/joerojas/Desarrollo/Curso-Basico-Phyton/102.misPaquetes/packages')
 
-print(path)
-
 import extra.iota
 
 print(extra.iota.funI())
---
+++ 102.misPaquetes/progs/main3.py
@@ -0,0 +1,7 @@
+from sys import path
+
+path.append('home/joerojas/Desarrollo/Curso-Basico-Phyton/102.misPaquetes/packages')
+
+import extra.good.best.sigma
+
+print(extra.good.best.sigma.funS())
